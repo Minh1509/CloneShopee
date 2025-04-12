74a6177938aa3300e28bc70fa700cc8d9648af20
Minh1509 <nguyenquangminh15092003@gmail.com>
2025-04-12 20:41:09 +0700

    fetch_api_register

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -11,6 +11,25 @@ class Http {
             }
         })
     }
+    get<T>(url: string, params?: any) {
+        return this.instance.get<T>(url, { params });
+    }
+
+    post<T>(url: string, data?: any) {
+        return this.instance.post<T>(url, data);
+    }
+
+    put<T>(url: string, data?: any) {
+        return this.instance.put<T>(url, data);
+    }
+
+    patch<T>(url: string, data?: any) {
+        return this.instance.patch<T>(url, data);
+    }
+
+    delete<T>(url: string) {
+        return this.instance.delete<T>(url);
+    }
 }
 
 const http = new Http()
